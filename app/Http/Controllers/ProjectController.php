22a55e6912d244abda3f308e3bec53bcab536977
HP <HP@DESKTOP-ODV6VHH>
2021-04-26 21:20:37 +0200

    second commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+
+class ProjectController extends Controller
+{
+    public function getProjects(){
+        $projects=Project::all();
+        return (view('projects',compact('projects')));
+
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Ahmed Lotfy</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="{{asset('site.webmanifest')}}">
+		<link rel="shortcut icon" type="image/x-icon" href="">
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}">
+
+
+		<!-- CSS here -->
+            <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+            <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+            <link rel="stylesheet" href="{{asset('css/slicknav.css')}}">
+            <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+            <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+            <link rel="stylesheet" href="{{asset('css/fontawesome-all.min.css')}}">
+            <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+            <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
+            <link rel="stylesheet" href="{{asset('css/slick.css')}}">
+            <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+            <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+            <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css')}}">
+
+   </head>
+
+   <body>
+      
+
+    <header>
+        <!-- Header Start -->
+       <div class="header-area header-sticky">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <!-- Logo -->
+                    <div class="col-xl-2 col-lg-1 col-md-1">
+                        <div class="logo">
+                        <a href="index"><img src="{{asset('img/logo/loder.png')}}" alt=""></a>
+                        </div>
+                    </div>
+                                         <!-- Top Bar Start -->
+    <div class="top-bar d-none d-md-block">
+
+<div class="col-md-6">
+    <div class="top-bar-right">
+        <div class="social">
+             <a href="https://www.facebook.com/ahmed.lotfy.92167789/"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://github.com/ahmedlotfy81199"><i class="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/ahmedlotfy81199/"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- Top Bar End -->
+                    <div class="col-xl-7 col-lg-8 col-md-6">
+                        <!-- Main-menu -->
+                        <div class="main-menu f-right d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">  
+                                    <li><a href="/">Home</a></li>
+                                    <li><a href="about">About</a></li>
+                                    <li><a href="projects">Projects</a></li>
+
+                                
+                                    </li>
+                                   
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>             
+                   
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+       </div>
+        <!-- Header End -->
+    </header>
+    <main>
+
+        <!-- slider Area Start-->
+        <div class="slider-area ">
+            <div class="slider-active">
+                <!-- Single Slider -->
+                <div class="single-slider slider-height d-flex align-items-center" >
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-9 col-lg-9 col-md-9">
+                                <div class="hero__caption">
+                     <img src="{{asset('img/hero/lotfy.jpg')}}"  ALIGN="right"  alt="">
+
+                                     <h1 >Hi!</h1>
+                                    <h2>Iam Ahmed Lotfy </h2>
+                                    <h3> Experienced Back-end developer</h3>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                
+
+       
+   
+	<!-- JS here -->
+	
+		<!-- All JS Custom Plugins Link Here here -->
+        <script src="./{{asset('js/vendor/modernizr-3.5.0.min.js')}}"></script>
+		<!-- Jquery, Popper, Bootstrap -->
+		<script src="./{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+        <script src="./{{asset('js/popper.min.js')}}"></script>
+        <script src="./{{asset('js/bootstrap.min.js')}}"></script>
+	    <!-- Jquery Mobile Menu -->
+        <script src="./{{asset('js/jquery.slicknav.min.js')}}"></script>
+
+		<!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="./{{asset('js/owl.carousel.min.js')}}"></script>
+        <script src="./{{asset('js/slick.min.js')}}"></script>
+        
+		<!-- One Page, Animated-HeadLin -->
+        <script src="./{{asset('js/wow.min.js')}}"></script>
+		<script src="./{{asset('js/animated.headline.js')}}"></script>
+        <script src="./{{asset('js/jquery.magnific-popup.js')}}"></script>
+
+		<!-- Scrollup, nice-select, sticky -->
+        <script src="./{{asset('js/jquery.scrollUp.min.js')}}"></script>
+        <script src="./{{asset('js/jquery.nice-select.min.js')}}"></script>
+		<script src="./{{asset('js/jquery.sticky.js')}}"></script>
+        
+        <!-- counter , waypoint -->
+        <script src="{{asset('http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js')}}"></script>
+        <script src="./{{asset('js/jquery.counterup.min.js')}}"></script>
+
+        <!-- counter -->
+        <script src="./{{asset('js/contact.js')}}"></script>
+        <script src="./{{asset('js/jquery.form.js')}}"></script>
+        <script src="./{{asset('js/jquery.validate.min.js')}}"></script>
+        <script src="./{{asset('js/mail-script.js')}}"></script>
+        <script src="./{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+        
+		<!-- Jquery Plugins, main Jquery -->	
+        <script src="./{{asset('js/plugins.js')}}"></script>
+        <script src="./{{asset('js/main.js')}}"></script>
+        
+    </body>
+</html>
